41960759bf11b0fe5726f97a63fec84f774a8e8b
sravantony2 <71973948+sravantony2@users.noreply.github.com>
2020-12-15 17:24:08 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -44,7 +44,7 @@ from tobrot import (
 from pyrogram import Client, Filters, MessageHandler, CallbackQueryHandler
 
 from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f, incoming_purge_message_f, \
-    incoming_gdrive_message_f, g_yt_playlist, rename_message_f, split_video, gp_link_generate, \
+    g_yt_playlist, rename_message_f, split_video, gp_link_generate, \
     incoming_gdrive_and_tg_message_f
 from tobrot.plugins.rclone_size import check_size_g, g_clearme
 from tobrot.plugins.status_message_fn import (
@@ -80,12 +80,6 @@ if __name__ == "__main__" :
         filters=Filters.command([f"{LEECH_COMMAND}"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_message_handler)
-    #
-    incoming_gdrive_message_handler = MessageHandler(
-        incoming_gdrive_message_f,
-        filters=Filters.command([f"{GLEECH_COMMAND}"]) & Filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(incoming_gdrive_message_handler)
     #
     incoming_telegram_download_handler = MessageHandler(
         down_load_media_f,
